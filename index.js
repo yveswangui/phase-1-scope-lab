3a9d94bd69f419759141b382ffa5b4f73ca3b17a
Yvonne <yveswangui@gmail.com>
2022-04-12 11:42:09 +0300

    Finish test

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'Jado'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Purvi'
+}
